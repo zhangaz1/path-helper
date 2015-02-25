cfca61436014d446776d90cb098429abb6053715
zhangaz1 <zhangaz1@hotmail.com>
2015-02-25 10:38:29 +0800

    update tests

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ var helper = require('./helper')();
 var assert = require('assert');
 
 var root = __dirname;
+var path = require('path');
 
-assert.equal(helper.resolve('abc').toLowerCase(), (root + '\\' + 'abc').toLowerCase());
-assert.equal(helper.resolve('./abc').toLowerCase(), (root + '\\' + 'abc').toLowerCase());
-// // assert.equal(helper.resolve('../abc').toLowerCase(), (root + '\\' + 'abc').toLowerCase());
+assert.equal(helper.resolve('abc').toLowerCase(), (root + path.sep + 'abc').toLowerCase());
+assert.equal(helper.resolve('./abc').toLowerCase(), (root + path.sep + 'abc').toLowerCase());
+// // assert.equal(helper.resolve('../abc').toLowerCase(), (root + path.sep + 'abc').toLowerCase());
 
 console.log('all tests passed!');
